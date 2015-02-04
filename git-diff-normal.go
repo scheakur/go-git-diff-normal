5454683883cf99cb2bad819505da9059a5008b8e
scheakur <scheakur@gmail.com>
2015-02-05 08:41:19 +0900

    Refactoring: Use []byte instead of string

--- git-diff-normal.go
+++ git-diff-normal.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -11,57 +12,76 @@ import (
 )
 
 func main() {
-	fmt.Println(formatNormal(gitDiff(os.Args[1:])))
+	fmt.Println(string(formatNormal(gitDiff(os.Args[1:]))))
 }
 
-func gitDiff(args []string) string {
+var (
+	re1   = regexp.MustCompile(`(?ms)\A.*?@@`)
+	repl1 = []byte("@@")
+	re2   = regexp.MustCompile(`(?m)^\+`)
+	repl2 = []byte("> ")
+	re3   = regexp.MustCompile(`(?m)^-`)
+	repl3 = []byte("< ")
+	re4   = regexp.MustCompile(`(?m)^(<.*)(\r|\n|\r\n)(>)`)
+	repl4 = []byte("$1$2---$2$3")
+	re5   = regexp.MustCompile(`(?m)^@@ -(\d+)(?:,(\d+))? \+(\d+)(?:,(\d+))? @@.*`)
+
+	add = []byte("a")
+	del = []byte("d")
+	mod = []byte("c")
+)
+
+func gitDiff(args []string) []byte {
 	base := strings.Fields("diff --no-index --no-color --no-ext-diff --unified=0")
 	params := append(base, args...)
 	out, err := exec.Command("git", params...).Output()
 	if err != nil && err.Error() != "exit status 1" {
 		log.Fatal(err)
 	}
-	return string(out)
+	return out
 }
 
-func formatNormal(unifiedDiff string) string {
+func formatNormal(unifiedDiff []byte) []byte {
 	diff := unifiedDiff
 
-	diff = regexp.MustCompile(`(?ms)\A.*?@@`).ReplaceAllString(diff, "@@")
-	diff = regexp.MustCompile(`(?m)^\+`).ReplaceAllString(diff, "> ")
-	diff = regexp.MustCompile(`(?m)^-`).ReplaceAllString(diff, "< ")
-	diff = regexp.MustCompile(`(?m)^(<.*)(\r|\n|\r\n)(>)`).ReplaceAllString(diff, "$1$2---$2$3")
+	diff = re1.ReplaceAll(diff, repl1)
+	diff = re2.ReplaceAll(diff, repl2)
+	diff = re3.ReplaceAll(diff, repl3)
+	diff = re4.ReplaceAll(diff, repl4)
 
-	re := regexp.MustCompile(`(?m)^@@ -(\d+)(?:,(\d+))? \+(\d+)(?:,(\d+))? @@.*`)
-	return re.ReplaceAllStringFunc(diff, func(src string) string {
-		m := re.FindStringSubmatch(src)
+	return re5.ReplaceAllFunc(diff, func(src []byte) []byte {
+		m := re5.FindSubmatch(src)
 		before := startEnd(m[1], m[2])
 		after := startEnd(m[3], m[4])
 		action := actionType(m[2], m[4])
-		return before + action + after
+		return append(append(before, action...), after...)
 	})
 }
 
-func startEnd(start, size string) string {
-	if size == "" || size == "0" {
+func startEnd(start, size []byte) []byte {
+	if len(size) == 0 || isZero(size) {
 		return start
 	}
 	end := num(start) + num(size) - 1
-	return fmt.Sprintf("%s,%d", start, end)
+	return []byte(fmt.Sprintf("%s,%d", start, end))
 }
 
-func actionType(beforeSize, afterSize string) string {
-	if beforeSize == "0" {
-		return "a"
+func actionType(beforeSize, afterSize []byte) []byte {
+	if isZero(beforeSize) {
+		return add
 	}
-	if afterSize == "0" {
-		return "d"
+	if isZero(afterSize) {
+		return del
 	}
-	return "c"
+	return mod
 }
 
-func num(str string) int64 {
-	n, err := strconv.ParseInt(str, 10, 64)
+func isZero(b []byte) bool {
+	return bytes.Equal(b, []byte("0"))
+}
+
+func num(str []byte) int64 {
+	n, err := strconv.ParseInt(string(str), 10, 64)
 	if err != nil {
 		log.Fatal(err)
 	}
